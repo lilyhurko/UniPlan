9e488eacdaad41c6c0e69cb8dc9379b9741e3259
Liliia Hurko <130755897+lilyhurko@users.noreply.github.com>
2026-03-09 12:19:10 +0100

    Initial Commit

---
+++ UniPlanTests/UniPlanTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniPlanTests.swift
+//  UniPlanTests
+//
+//  Created by Лілія Гурко on 09/03/2026.
+//
+
+import Testing
+@testable import UniPlan
+
+struct UniPlanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
